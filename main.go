d5fd4c1189352ab3d46afa461b2ee6084cc49e65
theun1c <belovdima1122@yandex.ru>
2025-11-14 02:55:35 +0300

    fix: delete atomics cse hd used mtx

--- main.go
+++ main.go
@@ -6,36 +6,18 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
-	"sync/atomic"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// с помощью респонс врайтера можно писать сетевые ответы на запросы
-	// принимает []byte
-	// w.Write()
-
-	str := "hello hello hello"
-	// преобразуем строку в слайс байтов
-	bytes := []byte(str)
-	// передаем и получаем ошибку
-	_, err := w.Write(bytes)
-
-	if err != nil {
-		fmt.Println("Error", err)
-	}
-}
-
-// используем атомик на переменной,
-// тк на эту переменную возможна гонка данных
-// а атомики быстрее мьютексов
-var money = atomic.Int64{} // денег в кармане
+// не нужно использовать атомик, поскольку секция с возможной
+// гонкой данных обрабатывается мьютексом
+var money int = 1000 // денег в кармане
 
 // с банком тоже самое
-var bank = atomic.Int64{} // денег в копилке
+var bank int = 0 // денег в копилке
 
 // создаем мьютекс для критической секции
 // в которой 2 разных горутины (хендлера)
-// работают с одним атомиком маней.
+// работают с одним атомиком маней. *уже не атомиком. я убрал атомики
 //
 // при одновременном входе в блоки с обработкой этой переменной
 // может случиться гонка данных, поскольку 2 горутины
@@ -69,12 +51,9 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 	// тот самый блок с возможной гонкой данных на переменной маней
 	// обращение из 2х независимых горутин к 1 переменной
 	mtx.Lock()
-	if money.Load()-int64(amount) >= 0 {
-		// у атомика нет метода минус, поэтому делаем через добавление -числа
-		money.Add(int64(-amount))
-		fmt.Println("Succsessfuly payment, now money is: ", money.Load())
-	} else {
-		fmt.Println("No money to pay. monay amount is: ", money.Load())
+	if money-amount >= 0 {
+		money -= amount
+		fmt.Println("Succsessfuly payment, now money is: ", money)
 	}
 	mtx.Unlock()
 }
@@ -99,34 +78,18 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	// тут тоже самое что и функции выше
 	// см. описание мьютекса и зачем он тут нужен
 	mtx.Lock()
-	if saveAmount > 0 && money.Load() >= int64(saveAmount) {
-		bank.Add(int64(saveAmount))
-		fmt.Println("Now bank is: ", bank.Load())
-		money.Add(int64(-saveAmount))
-		fmt.Println("Now money is: ", money.Load())
-	} else {
-		fmt.Println("cannot add this amount to bank")
+	if saveAmount > 0 && money >= saveAmount {
+		bank += saveAmount
+		fmt.Println("Now bank is: ", bank)
+		money -= saveAmount
+		fmt.Println("Now money is: ", money)
 	}
 	mtx.Unlock()
 }
 
 func main() {
-
-	// базовая сигнатура хендл функции
-	//
-	//http.HandleFunc("/", handler)
-
-	// кладем значение в переменную money,
-	// поскольку при объявлении атомика
-	// в него ничего нельзя класть
-	money.Add(1000)
-
-	// не нужно добавлять 0 =)
-	// bank.Add(0)
-
 	http.HandleFunc("/pay", payHandler)
 	http.HandleFunc("/save", saveHandler)
 
 	http.ListenAndServe(":8080", nil)
-
 }
